cfc28facd8680e9dfcfb530b84c69618be4fdb29
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-27 11:17:54 +0700

    Интеграция верхнего меню

--- local/templates/empty/components/bitrix/menu/top_menu/template.php
+++ local/templates/empty/components/bitrix/menu/top_menu/template.php
@@ -1,20 +1,9 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?if (!empty($arResult)):?>
-<ul class="left-menu">
-
-<?
-foreach($arResult as $arItem):
-	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
-		continue;
-?>
-	<?if($arItem["SELECTED"]):?>
-		<li><a href="<?=$arItem["LINK"]?>" class="selected"><?=$arItem["TEXT"]?></a></li>
-	<?else:?>
-		<li><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-	<?endif?>
-	
-<?endforeach?>
-
-</ul>
-<?endif?>
+<? if (!empty($arResult)) : ?>
+	<? foreach ($arResult as $arItem) : ?>
+		<li class="nav-item">
+			<a class="nav-link" href='<?= $arItem["LINK"] ?? "/" ?>'><?= $arItem["TEXT"] ?? "Главная" ?></a>
+		</li>
+	<? endforeach ?>
+<? endif ?>
--- local/templates/empty/header.php
+++ local/templates/empty/header.php
@@ -37,7 +37,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 </head>
 
 <body>
-<div id="panel">
+	<div id="panel">
 		<? $APPLICATION->ShowPanel(); ?>
 	</div>
 	<div class="main-wrapper" id="app">
@@ -49,16 +49,26 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 				</button>
 				<div class="collapse navbar-collapse" id="navbarResponsive">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a class="nav-link" href="dinosaurs.html">Динозавры</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="blog.html">Блог</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="shelter.html">Приют динозавриков</a>
-						</li>
-						
+						<? $APPLICATION->IncludeComponent(
+							"bitrix:menu",
+							"top_menu",
+							array(
+								"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+								"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+								"DELAY" => "N",	// Откладывать выполнение шаблона меню
+								"MAX_LEVEL" => "1",	// Уровень вложенности меню
+								"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+									0 => "",
+								),
+								"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+								"MENU_CACHE_TYPE" => "N",	// Тип кеширования
+								"MENU_CACHE_USE_GROUPS" => "N",	// Учитывать права доступа
+								"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+								"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+							),
+							false
+						); ?>
+
 						<li class="nav-item">
 							<a class="btn btn-secondary" href="auth.html">Войти</a>
 							<a class="btn btn-outline-secondary" href="register.html">Зарегистрироваться</a>
@@ -77,6 +87,5 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 					</ul>
 				</div>
 			</div>
-			
+
 		</nav>
-		
