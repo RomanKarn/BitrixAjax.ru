2ff728ffd8274c3e9c9376d9482c35d18f8f00e1
Роман Карнаухов <WorkKarn@yandex.ru>
2024-05-28 13:14:09 +0700

    По качто все работатет

---
+++ search-dino/index.php
@@ -0,0 +1,35 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Поиск");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:search.page",
+	"",
+	Array(
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "N",
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "N",
+		"CHECK_DATES" => "N",
+		"DEFAULT_SORT" => "rank",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FILTER_NAME" => "",
+		"NO_WORD_LOGIC" => "N",
+		"PAGER_SHOW_ALWAYS" => "Y",
+		"PAGER_TEMPLATE" => "",
+		"PAGER_TITLE" => "Результаты поиска",
+		"PAGE_RESULT_COUNT" => "50",
+		"RESTART" => "Y",
+		"SHOW_WHEN" => "N",
+		"SHOW_WHERE" => "Y",
+		"USE_LANGUAGE_GUESS" => "Y",
+		"USE_SUGGEST" => "N",
+		"USE_TITLE_RANK" => "N",
+		"arrFILTER" => array("iblock_content"),
+		"arrFILTER_iblock_content" => array("2"),
+		"arrWHERE" => array()
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- test.php
+++ test.php
@@ -1,7 +1,22 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("test");
-?>
-
-фывыф
-<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:search.title",
+	"",
+	Array(
+		"CATEGORY_0" => array("iblock_content"),
+		"CATEGORY_0_TITLE" => "",
+		"CATEGORY_0_iblock_content" => array("2"),
+		"CHECK_DATES" => "N",
+		"CONTAINER_ID" => "title-search",
+		"INPUT_ID" => "title-search-input",
+		"NUM_CATEGORIES" => "1",
+		"ORDER" => "date",
+		"PAGE" => "#SITE_DIR#search-dino/index.php",
+		"SHOW_INPUT" => "Y",
+		"SHOW_OTHERS" => "N",
+		"TOP_COUNT" => "5",
+		"USE_LANGUAGE_GUESS" => "Y"
+	)
+);?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
